539df043db641de4be45900a043afbce45a9a54a
Shinh0707 <www.shin0707@gmail.com>
2024-09-07 10:20:52 +0900

    ライティング調整

--- Assets/SceneGroup/MazeScene/Scripts/TileBased2DLight.cs
+++ Assets/SceneGroup/MazeScene/Scripts/TileBased2DLight.cs
@@ -1,7 +1,9 @@
+using SL.Lib;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 public class TileBased2DLight : MonoBehaviour
 {   
     [SerializeField] private GameObject Tile3D;
@@ -68,9 +70,23 @@ public class TileBased2DLight : MonoBehaviour
 
     private void UpdateTiles()
     {
+        SetUnactiveAllTiles();
         var centerTileIndex = MazeGameScene.Instance.MazeManager.GetTensorPosition(LightCenter);
         int lfs = LightFieldSize();
         MazeGameScene.Instance.MazeManager.BoundRange(centerTileIndex.i - lfs - 1, lfs * 2 + 1, centerTileIndex.j - lfs - 1, lfs * 2 + 1, UpdateTile);
+        var bM = MazeGameScene.Instance.MazeManager.GetBaseMap();
+        ESEResult ESE = ESEResult.Get(new Indice[] { centerTileIndex.i, centerTileIndex.j }, bM);
+        var r = ESE.Extract(bM,1);
+        Debug.Log($"{r}");
+        (int i, int j) offset = (centerTileIndex.i - 1, centerTileIndex.j - 1);
+        Vector3 halfOffset = Vector2.one * 0.5f;
+        foreach (var index in new (int i,int j)[] { (0, 0), (2, 0), (0, 2), (2, 2) })
+        {
+            if (r[index.i, index.j] == 0 && r[1,index.j]==1 && r[index.i,1]==1)
+            {
+                PushTile(MazeGameScene.Instance.MazeManager.GetTilePosition((index.i+offset.i,index.j+offset.j))+halfOffset);
+            }
+        }
     }
 
     private void SetUnactiveAllTiles()
